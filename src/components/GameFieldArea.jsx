e60a59aa06715b0200d88d8c3968b834f2a8520a
SzumielDev <107126687+SzumielDev@users.noreply.github.com>
2023-04-21 19:41:58 +0200

    21.04.2023

--- src/components/GameFieldArea.jsx
+++ src/components/GameFieldArea.jsx
@@ -34,28 +34,31 @@ const GameFieldArea = () => {
     //2 player = 1
     const [player, setPlayer] = useState(0);
 
-    //Definde moves
-    const [playerOne, setPlayerOne] = useState([]);
-    const [playerTwo, setPlayerTwo] = useState([]);
+    //Set Game Status
+    //0 = not started
+    //1 = game in progress
+    //2 = game ended winner 1
+    //3 = game ended winner 2
+    //4 = game over
+    const [gameStatus, setGameStatus] = useState(1);
 
-    //Update Game Area every move
-    const updateGame = (id) => {
-        let closedFields = gameArea.map (i => i.player)
-
-        for (let i = 1; i <= 7; i++) {
-            const [posA, posB, posC] = gameWiner[i]; 
-            const valueOne = closedFields[posA];
-            const valueTwo = closedFields[posB];
-            const valueThree = closedFields[posC];
-
-
-            if (valueOne === 0 && valueTwo === 0 && valueThree === 0) {
-                console.log("wygrywa gracz 1")
-            } else if (valueOne === 1 && valueTwo === 1 && valueThree === 1) {
-                console.log("wygrywa gracz 2")
-            }
+    const checkGameStatus = (id) => {
+        if (gameStatus === 0) {
+            console.log("Wystartuj grę")
+        } else if (gameStatus === 1) {
+            console.log("Gra w toku")
+            updateGame(id);
+        } else if (gameStatus === 2) {
+            console.log("Wygrywa gracz 1")
+        } else if (gameStatus === 3) {
+            console.log("Wygrywa gracz 2")
+        } else {
+            console.log("Koniec gry, nie ma wygranych. Kliknij restart aby zagrać ponownie.")
         }
+    }
 
+    //Start/Update Game Area every move
+    const updateGame = (id) => {
         const updateGameArea = gameArea.map(field => {
         if (field.id === id) {
             if (player === 0) {
@@ -75,17 +78,45 @@ const GameFieldArea = () => {
         return field;
         });
         setGameArea(updateGameArea);
-        updatePlayer(id);
+        checkFieldStatus(id);
     }
 
-    const updatePlayer = (id) => {
+    //Check if someone win the game
+    const checkFieldStatus = (id) => {
+        let closedFields = gameArea.map (i => i.player)
+        console.log(closedFields)
+
+        for (let [a, b, c] of gameWiner) {
+            const fieldA = a - 1;
+            const fieldB = b - 1;
+            const fieldC = c - 1;
+
+            if (closedFields[fieldA] === 0 && closedFields[fieldB] === 0 && closedFields[fieldC] === 0) {
+                setGameStatus(2)
+                break;
+            } else if (closedFields[fieldA] === 1 && closedFields[fieldB] === 1 && closedFields[fieldC] === 1) {
+                setGameStatus(3)
+                break;
+            } 
+        }
+        updatePlayer();
+    }
+
+    const updatePlayer = () => {
     //Change player
         if (player === 0) {
             setPlayer(1);
-            setPlayerOne(current => [...current, id]);
         } else {
             setPlayer(0);
-            setPlayerTwo(current => [...current, id]);
+        }
+        checkGameOver();
+    }
+
+    //Check that each field is filled
+    const checkGameOver = () => {
+        const playerNotEmpty = gameArea.every((i) => !!i.player);
+        if (playerNotEmpty) {
+            setGameStatus(4)
         }
     }
 
@@ -101,9 +132,7 @@ const GameFieldArea = () => {
             return {...i, src: '', player: ''};
         });
         setGameArea(resetGameArea);
-        //reset Scores
-        setPlayerOne([]);
-        setPlayerTwo([]);
+        setGameStatus(1);
     }
 
     return (
@@ -114,7 +143,7 @@ const GameFieldArea = () => {
                         if (field.src !== "") {
                             closeFunction(field.id);
                         } else {
-                            updateGame(field.id)
+                            checkGameStatus(field.id)
                         }
                     }} className="game-item"><img className="filler" src={field.src} /></div>
                 ))}
